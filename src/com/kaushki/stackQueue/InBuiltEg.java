ca75234b99549968ce4a0c300c12aa7714363845
Kaushki <kaushkikumari1110@gmail.com>
2026-09-01 08:39:29 +0530

    learning stack and queue

---
+++ src/com/kaushki/stackQueue/InBuiltEg.java
@@ -0,0 +1,44 @@
+package com.kaushki.stackQueue;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class InBuiltEg {
+
+        static void main() {
+            /*
+            Stack<Integer> stack = new Stack<>();
+            stack.push(34);
+            stack.push(14);
+            stack.push(67);
+            stack.push(89);
+            stack.push(86);
+
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+            System.out.println(stack.pop());
+
+
+             */
+
+            /*
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(3);
+            queue.add(6);
+            queue.add(5);
+            queue.add(19);
+            queue.add(1);
+
+            System.out.println(queue.peek());
+            System.out.println(queue.remove());
+
+             */
+
+            Deque
+        }
+
+
+}
