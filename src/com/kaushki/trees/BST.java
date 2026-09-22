16bd71aeb746f93c4c956faf315507dc635e5a50
Kaushki <kaushkikumari1110@gmail.com>
2026-09-22 12:10:39 +0530

    Learnt Binary Search Tree and its implementation

---
+++ src/com/kaushki/trees/BST.java
@@ -0,0 +1,44 @@
+package com.kaushki.trees;
+
+class BST {
+    public class Node{
+        private int value;
+        int height;
+        private Node left;
+        private Node right;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+    private Node root;
+    public BST() {
+
+    }
+    public int height (Node node){
+        if(node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+    public boolean isEmpty() {
+        return root == null;
+    }
+    public void display() {
+        display(root,"Root Node: ");
+    }
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.getValue());
+
+        display(node.left,"Left child of " + node.getValue() + " : ");
+        display(node.left,"Right child of " + node.getValue() + " : ");
+
+    }
+}
--- src/com/kaushki/trees/Main.java
+++ src/com/kaushki/trees/Main.java
@@ -4,10 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
+        /*
         Scanner scanner = new Scanner(System.in);
         BinaryTrees tree = new BinaryTrees();
         tree.populate(scanner);
         tree.display();
         tree.prettyDisplay();
+
+         */
+        insertionBST tree = new insertionBST();
+        int[] nums = {5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
+        // If the array is sorted, the tree created will be unbalanced
+        tree.populate(nums);
+        tree.display();
     }
 }
